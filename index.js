8e94e87043e7f0828cecceddec357e90e3ebdf5a
Kevin Griffin <griffin.kev@gmail.com>
2022-03-17 10:33:18 -0400

    adds basic spec format and style

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 const fs = require('fs');
 const showdown = require('showdown');
-const converter = new showdown.Converter();
+const replace = require('replace-in-file');
 
+const classMap = {
+    h1: 'ui large header',
+    h2: 'ui medium header',
+    h3: 'ui small header',
+    h4: 'ui smallest header',
+    ul: 'ui list',
+    li: 'ui item'
+}
+
+const bindings = Object.keys(classMap)
+    .map(key => ({
+        type: 'output',
+        regex: new RegExp(`<${key}(.*)>`, 'g'),
+        replace: `<${key} class='${classMap[key]}' $1>`
+    }));
+
+const converter = new showdown.Converter({
+    extensions: [...bindings]
+});
 
 function writeHTML(data) {
-    console.log("📝 writing docs");
-    fs.writeFile('./docs/index.html', converter.makeHtml(data), err => {
-        if (err) {
-            console.error(err);
+    console.log('📝 writing docs');
+    fs.copyFileSync('./docs/.index.html', './docs/index.html');
+    console.log(data)
+
+    const options = {files: './docs/index.html', from: /<--- spec-body --->/g, to: converter.makeHtml(data),};
+    replace(options, (error, results) => {
+        if (error) {
+            return console.error('Error occurred:', error);
         }
-        console.log("✅ All done!\n");
-    })
+        console.log('✅ All done!\n', results);
+    });
 }
 
 const readStream = fs.createReadStream('./spec.md', 'utf8');
 let data = '';
-console.log("📖 reading spec")
-readStream.on('data', function(chunk) {
+console.log('📖 reading spec')
+readStream.on('data', function (chunk) {
     data += chunk;
-}).on('end', function() {
+}).on('end', function () {
     writeHTML(data);
 });
--- spec.md
+++ spec.md
@@ -1,9 +1,49 @@
-# my spec
+# title: "TODO - Your title"
 
-updated from hackmd
+### docname: draft-todo-your-doc-name 
+    
+    v: 3 
+    area: AREA 
+    workgroup: WG Working Group 
+    keywords: KEYWORDS venue:
 
-more content
+    group: WG
+    type: Working Group
+    mail: WG@example.com
+    arch: https://example.com/WG
+    github: USER/REPO
+    latest: https://example.com/LATEST
 
-additional content
+## author:
 
-more content
+    fullname: Your Name Here
+    organization: Your Organization Here
+    email: your.email@example.com
+
+## normative:
+
+## informative:
+
+## Abstract
+
+TODO Abstract
+
+# Introduction
+
+TODO Introduction
+
+# Conventions and Definitions
+
+TODO Conventions and Definitions
+
+# Security Considerations
+
+TODO Security
+
+# IANA Considerations
+
+This document has no IANA actions.
+
+# Acknowledgments
+
+TODO acknowledge.
